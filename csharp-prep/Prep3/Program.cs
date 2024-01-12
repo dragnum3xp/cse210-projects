1579a17d3eb9dc6246422ffd4e82c820abeedd4d
dragnum3xp <320235583@byui.edu>
2024-01-12 09:40:55 -0300

    Activity 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        Console.WriteLine("What is the magic number?");
+        int guess;
+        int attempts = 0;
+        
+        do
+        {
+            attempts += 1;
+            Console.WriteLine("What is your guess?");
+            guess = Convert.ToInt32(Console.ReadLine());
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (guess != number);
+
+        if (guess == number)
+        {
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It tooks {attempts} attempts");
+        }
     }
 }
