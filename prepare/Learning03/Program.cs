91debd61a8eab74a18a86878ed8410d7340704fe
dragnum3xp <320235583@byui.edu>
2024-01-23 09:51:37 -0300

    fraction created

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,29 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction math1 = new Fraction();
+        
+        Console.WriteLine(math1.GetFractionString());
+        Console.WriteLine(math1.GetDecimalValue());
+        
+        Fraction math2 = new Fraction(5);
+        Console.WriteLine(math2.GetFractionString());
+        Console.WriteLine(math2.GetDecimalValue());
+
+        Fraction math3 = new Fraction(3, 4);
+        Console.WriteLine(math3.GetFractionString());
+        Console.WriteLine(math3.GetDecimalValue());
+
+        Fraction math4 = new Fraction(1, 3);
+        Console.WriteLine(math4.GetFractionString());
+        Console.WriteLine(math4.GetDecimalValue());
+
+        
+        }
     }
-}
