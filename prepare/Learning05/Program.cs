e67a1b34555617eb374a049474acc89d7590aa5f
dragnum3xp <320235583@byui.edu>
2024-02-07 10:30:08 -0300

    Learning activity week 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -3,7 +3,24 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning05 World!");
+    {   
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("blue", 5);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("red", 5, 3);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("green", 6);
+        shapes.Add(c1);
+
+        foreach (Shape shape in shapes){
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }   
     }
 }
